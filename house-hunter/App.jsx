6f3f70552e33570f52a6cdbf8f4495cf3b6118ca
rilixzy4 <rilickzy4@gmail.com>
2025-06-05 14:24:40 +0100

    Add files via upload

---
+++ house-hunter/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import PropertyGrid from './components/PropertyGrid';
+import Signup from './components/Signup';
+import Login from './components/Login';
+
+export default function App() {
+
+const[name, setName] = useState('')
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+  const response = await axios.post({name})
+  console.log(response)
+}
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="min-h-screen font-sans">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={
+              <>
+                <Hero />
+                <PropertyGrid />
+              </>
+            } />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
