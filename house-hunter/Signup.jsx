128f857723e11f0b9e0891c8f4106499ffc2a75d
G0odybag <129758368+G0odybag@users.noreply.github.com>
2025-06-23 16:34:42 +0100

    Update Signup.jsx

--- house-hunter/Signup.jsx
+++ house-hunter/Signup.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from '../axiosInstance';
 
-const Signup = () => {
+export default function Signup() => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [password, setPassword] = useState('');
